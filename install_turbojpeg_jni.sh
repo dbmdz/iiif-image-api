915a78857af8e25f6e97ad9a560d3ca8c6a7408b
Johannes Baiter <johannes.baiter@bsb-muenchen.de>
2016-11-15 18:37:04 +0100

    Don't include libjpeg-turbo in JAR, add installation script

---
+++ install_turbojpeg_jni.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+JAVA_HOME=$(readlink -f `which javac` | sed "s:/bin/javac::")
+
+cd libjpeg-turbo
+JNI_CFLAGS="-I$JAVA_HOME/include -I/usr/include -I$JAVA_HOME/include/linux" ./configure --with-java
+make
+cp .libs/libturbojpeg.so.0.1.0 /usr/lib/libturbojpeg-jni.so
