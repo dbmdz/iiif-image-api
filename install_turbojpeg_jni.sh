1334137f9ddf0b2f0cf46ea89c079bfa684793d9
Johannes Baiter <johannes.baiter@bsb-muenchen.de>
2016-11-16 10:36:59 +0100

    Update turbojpeg installation script

--- install_turbojpeg_jni.sh
+++ install_turbojpeg_jni.sh
@@ -3,6 +3,7 @@
 JAVA_HOME=$(readlink -f `which javac` | sed "s:/bin/javac::")
 
 cd libjpeg-turbo
+autoreconf -fiv
 JNI_CFLAGS="-I$JAVA_HOME/include -I/usr/include -I$JAVA_HOME/include/linux" ./configure --with-java
 make
 cp .libs/libturbojpeg.so.0.1.0 /usr/lib/libturbojpeg-jni.so
